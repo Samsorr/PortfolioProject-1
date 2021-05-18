fc1056e2d9281b5edd8697fa11ccf88a5182fb85
samsorultra <samsor13@icloud.com>
2021-05-18 19:00:14 +0200

    user and usertest aangepast

--- Test/UserTest.java
+++ Test/UserTest.java
@@ -2,11 +2,20 @@ import static org.junit.Assert.*;
 
 public class UserTest {
 
+    User samsor = new User("sam", "samsor13@icloud.com", "lol");
+    User julia = new User("juliadejong123456", "samsor13@icloud.com", "lol");
+
 
     @org.junit.Test
     public void constructorTest(){
-        User samsor = new User("sam", "samsor13@icloud.com", "lol");
-        assertEquals(samsor, samsor);
+        assertEquals(samsor, samsor);           //Testen of het juiste object wordt aangemaakt.
     }
+
+    @org.junit.Test
+    public void gebruikersnaamLengthTest(){
+        assertEquals("juliadejong1234", julia.getGebruikersnaam());           //Testen of het gemaakte object een gebruikersnaam heeft met 15 karakters of minder.
+    }
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        User julia = new User("juliadejong123456", "samsor13@icloud.com", "lol");
+        System.out.println(julia.getGebruikersnaam());
     }
 }
